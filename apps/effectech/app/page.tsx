73f340594a70195770681e4c1ccae6b4b237a054
Darnell Noel <darnellnoel.webdev@gmail.com>
2023-09-14 00:24:02 -0400

    chore: update app directory names and gitignore

--- apps/effectech/app/page.tsx
+++ apps/effectech/app/page.tsx
@@ -55,8 +55,9 @@ export default function Page(): JSX.Element {
       <div className={styles.description}>
         <p>
           examples/basic&nbsp;
-          <code className={styles.code}>docs</code>
+          <code className={styles.code}>web</code>
         </p>
+        <p>Effectech Page</p>
         <div>
           <a
             href="https://vercel.com?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"
